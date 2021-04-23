f67d91f2aa0f3e8013b0bbd14b1cde0bfd6dfe55
amehlhase316 <amehlhase316@asu.edu>
2021-04-23 14:24:31 -0700

    Remove Calc part from Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,6 @@ public class Main {
 
         System.out.println("Hello to your Final Exam Spring C 2020.");
         System.out.println("You got this!");
-
-        Calc some = new Calc();
-        System.out.println("Subtracting stuff: " + some.Subtract(new int[]{1, 5, 3}));
-    }
+ }
 
 }
