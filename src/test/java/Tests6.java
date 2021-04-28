b7bde1179a4e1c67c8cf2102cb1aec27f21d9c0e
Andrew Pappas <ampappa3@asu.edu>
2021-04-28 11:43:49 -0700

    bb test complete trying to compile

--- src/test/java/Tests6.java
+++ src/test/java/Tests6.java
@@ -10,6 +10,7 @@ import static org.junit.Assert.*;
 // This test class does not really do anything that makes sense, it just 
 // creates one test that passes and one that fails. 
 public class Tests {
+    Calc calc  = new Calc();
 
     //whitebox test
 	@Test
@@ -20,12 +21,13 @@ public class Tests {
     //blackbox test to see if negative numbers work as well as positive values
     @Test
     public void bb1() {
+    
         //ans = subtract 1-1, 1-5, 1-3
-        int ans = Subtract(new int[]{1, 5, 3});
+        int ans = calc.Subtract(new int[]{1, 5, 3});
         assertEquals(-6, ans, .01);
         
         //test positive values
-        int ans1 = Subtract(new int[]{10, 5, 3});
+        int ans1 = calc.Subtract(new int[]{10, 5, 3});
         assertEquals(12, ans, .01);
     }
 
